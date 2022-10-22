e929c42b3cee90de46190d2be5c7ea8d31e57253
Zain Raza <sraza@ptc.com>
2022-10-22 17:19:24 -0400

    Add solution for 'standalone' section of tutorial

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 
 export default {
-    input: 'web/js/index.js',
+    input: 'standalone/js/index.js',
     output: {
         file: 'dist/main.js',
         format: 'iife'
