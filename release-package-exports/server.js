26808eb4d259171db2cce7a10a40029f1cd9f1ee
Zain Raza <sraza@ptc.com>
2023-01-30 08:29:57 -0500

    Add the good \'ol 'Hello World' template :)

---
+++ release-package-exports/server.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('trust proxy', 1); // To allow to run correctly behind Heroku
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+
+// Controller functions
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'html', 'index.html'));
+});
+
+module.exports = app;
