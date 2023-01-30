a174b4f86c35dbb05a84ed7728bf8cc2c13239ed
Zain Raza <sraza@ptc.com>
2023-01-30 11:45:32 -0500

    encapsulate auth middleware into separate module

--- gltf-viewer-oauth/app.js
+++ gltf-viewer-oauth/app.js
@@ -1,3 +1,4 @@
+// Imports
 const path = require('path');
 const uuid = require('uuid');
 
@@ -5,29 +6,25 @@ const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 
-// const RedisStore = require('connect-redis')(session);  // [Zain] old stuff
 const MemoryStore = require('memorystore')(session);
 const passport = require('passport');
-// const OnshapeStrategy = require('passport-onshape');
-const OAuth2Strategy = require('passport-onshape');
 
+var authentication = require('./authentication');
 const config = require('./config');
 
-// const redisClient = require('./redis-client');  // [Zain] old stuff
-
+// Create the app
 const app = express();
+authentication.init();
 
+// Middleware - rendering static files
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use(bodyParser.json());
 
+// Middleware - auth
 app.set('trust proxy', 1); // To allow to run correctly behind Heroku
 
 app.use(session({
-    // [Zain] old stuff
-    // store: new RedisStore({
-    //     client: redisClient
-    // }),
     store: new MemoryStore({
         checkPeriod: 86400000 // prune expired entries every 24h
     }),
@@ -45,41 +42,9 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-// ***** OLD VERSION ***
-// passport.use(
-//     new OnshapeStrategy({
-//         clientID: config.oauthClientId,
-//         clientSecret: config.oauthClientSecret,
-//         callbackURL: config.oauthCallbackUrl,
-//         authorizationURL: `${config.oauthUrl}/oauth/authorize`,
-//         tokenURL: `${config.oauthUrl}/oauth/token`,
-//         userProfileURL: `${config.oauthUrl}/api/users/sessioninfo`
-//     },
-//     (accessToken, refreshToken, profile, done) => {
-//         profile.accessToken = accessToken;
-//         profile.refreshToken = refreshToken;
-//         return done(null, profile);
-//     }
-// ));
-// *** NEW CHANGE - Zain ***
-passport.use(new OAuth2Strategy({
-    authorizationURL: 'https://oauth.onshape.com/oauth/authorize',
-    tokenURL: 'https://oauth.onshape.com/oauth/token',
-    clientID: config.oauthClientId,
-    clientSecret: config.oauthClientSecret,
-    callbackURL: config.oauthCallbackUrl
-  },
-  (accessToken, refreshToken, profile, done) => {
-        profile.accessToken = accessToken;
-        profile.refreshToken = refreshToken;
-        return done(null, profile);
-    }
-));
-passport.serializeUser((user, done) => done(null, user));
-passport.deserializeUser((obj, done) => done(null, obj));
 
-// TODO[Zain] - see Slack dm to Ron, the error is somewhere here - we go to signin/then redirect after we have the code
-// BUT then for some reason the redirection fails and we do not get the access token - resulting in an error
+
+// Routes
 app.use('/oauthSignin', (req, res) => {
     const state = {
         docId: req.query.documentId,
