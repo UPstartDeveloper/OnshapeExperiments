981fd672c9dc8380cd496f56b577d07478181450
Zain Raza <sraza@ptc.com>
2023-05-12 15:01:07 -0400

    remove 'ttl' from 'appSettings' variable

--- release-package-exports/raza-client.js
+++ release-package-exports/raza-client.js
@@ -1,17 +1,17 @@
 const { LRUCache } = require('lru-cache');
 
+/* In-memory storage for configuration settings set by the user. */
+let appSettings = new LRUCache({ max: 500 });
 
-const options = {
+/* In-memory storage for translation jobs. */
+const translatedFilesOptions = {
     // At least one of 'max', 'ttl', or 'maxSize' is required, to prevent unsafe unbounded storage.
     max: 500,  
     // how long to live in ms
     ttl: 1000 * 60 * 5, // 5 min, since we're just testing
 };
 
-/* In-memory storage for configuration settings set by the user. */
-let appSettings = new LRUCache(options);
-/* In-memory storage for translation jobs. */
-let translatedFiles = new LRUCache(options);
+let translatedFiles = new LRUCache(translatedFilesOptions);
 
 module.exports = {
     "appSettings": appSettings,
